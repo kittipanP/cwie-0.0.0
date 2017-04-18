6f7fdad71d937d4feedaae3e1cfca50f64df7a65
KITTIPAN <kittipan.prasertsang@gmail.com>
2017-04-18 11:13:53 +0700

    delete old

--- ui_connect/student_management/insert/add_more_file_using_jquery/index.php
+++
@@ -1,9 +0,0 @@
-<div class="field_wrapper">
-	<div>
-    	<input type="text" name="field_name[]" value=""/>
-        <a href="javascript:void(0);" class="add_button" title="Add field"><i class="fa fa-plus-square"></i></a>
-    </div>
-</div>
-
-
-						  
--- web_elements/footer.php
+++
@@ -1,8 +0,0 @@
-
-    <footer class="w3-container w3-theme-d3 w3-padding-16">
-      <h5>Footer</h5>
-    </footer>
-
-    <footer class="w3-container w3-theme-d5" >
-       <p>By <a href="#" target="_blank">WD Trainee Team</a></p>
-    </footer>
